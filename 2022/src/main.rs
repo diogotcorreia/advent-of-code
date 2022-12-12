54573358d9c9f74f0b7cc5452cec989ae5960a42
Diogo Correia <me@diogotc.com>
2022-12-12 13:18:23 +0000

    Add argument to run only a certain part to 2022

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -22,6 +22,10 @@ fn main() -> Result<()> {
         .expect("Please provide a the day number")
         .parse()
         .expect("Puzzle day must be a number");
+    let part_number: Option<usize> = env::args()
+        .nth(2)
+        .map(|x| x.parse().expect("Part must be a number"))
+        .filter(|&x| x == 1 || x == 2);
 
     let input_file = format!("inputs/day{:0>2}.txt", puzzle_index);
     let input = BufReader::new(File::open(input_file)?)
@@ -29,18 +33,18 @@ fn main() -> Result<()> {
         .map(|r| r.expect("I/O error while reading input"));
 
     match puzzle_index {
-        1 => run_day(day01::AocDay01::preprocessing(input))?,
-        2 => run_day(day02::AocDay02::preprocessing(input))?,
-        3 => run_day(day03::AocDay03::preprocessing(input))?,
-        4 => run_day(day04::AocDay04::preprocessing(input))?,
-        5 => run_day(day05::AocDay05::preprocessing(input))?,
-        6 => run_day(day06::AocDay06::preprocessing(input))?,
-        7 => run_day(day07::AocDay07::preprocessing(input))?,
-        8 => run_day(day08::AocDay08::preprocessing(input))?,
-        9 => run_day(day09::AocDay09::preprocessing(input))?,
-        10 => run_day(day10::AocDay10::preprocessing(input))?,
-        11 => run_day(day11::AocDay11::preprocessing(input))?,
-        12 => run_day(day12::AocDay12::preprocessing(input))?,
+        1 => run_day(day01::AocDay01::preprocessing(input), part_number)?,
+        2 => run_day(day02::AocDay02::preprocessing(input), part_number)?,
+        3 => run_day(day03::AocDay03::preprocessing(input), part_number)?,
+        4 => run_day(day04::AocDay04::preprocessing(input), part_number)?,
+        5 => run_day(day05::AocDay05::preprocessing(input), part_number)?,
+        6 => run_day(day06::AocDay06::preprocessing(input), part_number)?,
+        7 => run_day(day07::AocDay07::preprocessing(input), part_number)?,
+        8 => run_day(day08::AocDay08::preprocessing(input), part_number)?,
+        9 => run_day(day09::AocDay09::preprocessing(input), part_number)?,
+        10 => run_day(day10::AocDay10::preprocessing(input), part_number)?,
+        11 => run_day(day11::AocDay11::preprocessing(input), part_number)?,
+        12 => run_day(day12::AocDay12::preprocessing(input), part_number)?,
         _ => unimplemented!("Unknown puzzle"),
     };
 
@@ -53,9 +57,16 @@ pub trait AocDay<R1: Display, R2: Display> {
     fn part2(&self) -> R2;
 }
 
-fn run_day<T: AocDay<R1, R2>, R1: Display, R2: Display>(puzzle: T) -> Result<()> {
-    println!("Part 1: {}", puzzle.part1());
-    println!("Part 2: {}", puzzle.part2());
+fn run_day<T: AocDay<R1, R2>, R1: Display, R2: Display>(
+    puzzle: T,
+    part_number: Option<usize>,
+) -> Result<()> {
+    if part_number.unwrap_or(1) == 1 {
+        println!("Part 1: {}", puzzle.part1());
+    }
+    if part_number.unwrap_or(2) == 2 {
+        println!("Part 2: {}", puzzle.part2());
+    }
 
     Ok(())
 }
