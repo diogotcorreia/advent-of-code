30137ed7c728f81d8711f5909bb1c71eaeeb8e88
Diogo Correia <me@diogotc.com>
2024-12-01 11:46:29 +0100

    2024: add day 1

--- 2024/src/day01.rs
+++ 2024/src/day01.rs
@@ -1,20 +1,41 @@
 use aoc_common::AocDay;
+use itertools::Itertools;
 
 pub struct AocDay01 {
-    // TODO
+    list_l: Vec<u32>,
+    list_r: Vec<u32>,
 }
 
 impl AocDay<u32, u32> for AocDay01 {
     fn preprocessing(lines: impl Iterator<Item = String>) -> Self {
-        // TODO
+        let (mut list_l, mut list_r): (Vec<_>, Vec<_>) = lines
+            .map(|l| {
+                let (l, r) = l.split_once(" ").expect("line to have two numbers");
+                let l = l.trim().parse::<u32>().expect("number to be valid number");
+                let r = r.trim().parse::<u32>().expect("number to be valid number");
+                (l, r)
+            })
+            .unzip();
 
-        AocDay01 { /* TODO */ }
+        list_l.sort();
+        list_r.sort();
+
+        AocDay01 { list_l, list_r }
     }
     fn part1(&self) -> u32 {
-        todo!()
+        (self.list_l)
+            .iter()
+            .zip(&self.list_r)
+            .map(|(l, r)| l.abs_diff(*r))
+            .sum()
     }
     fn part2(&self) -> u32 {
-        todo!()
+        let counts = self.list_r.iter().counts();
+
+        self.list_l
+            .iter()
+            .map(|l| l * (*counts.get(l).unwrap_or(&0) as u32))
+            .sum()
     }
 }
 
@@ -22,17 +43,17 @@ impl AocDay<u32, u32> for AocDay01 {
 mod day01tests {
     use super::*;
 
-    const INPUT: &[&str] = &[""];
+    const INPUT: &[&str] = &["3   4", "4   3", "2   5", "1   3", "3   9", "3   3"];
 
     #[test]
     fn part1() {
         let day = AocDay01::preprocessing(INPUT.iter().map(|x| String::from(*x)));
-        assert_eq!(day.part1(), todo!());
+        assert_eq!(day.part1(), 11);
     }
 
     #[test]
     fn part2() {
         let day = AocDay01::preprocessing(INPUT.iter().map(|x| String::from(*x)));
-        assert_eq!(day.part2(), todo!());
+        assert_eq!(day.part2(), 31);
     }
 }
